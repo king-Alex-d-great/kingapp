7d4e187aa2eaac94c274fadf5ef84b496d274c94
ogubuike alex <ogubuikealex@gmail.com>
2020-10-13 02:27:29 +0100

    navbar started

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+class Navbar extends Component {
+    state = {  }
+    render() { 
+        return ( 
+            <nav>
+                <ul>
+                    <li></li>
+                     <li></li>
+                      <li></li> 
+                      <li></li>
+                      <li></li>
+                    <div className="build">
+                        <p>build a product with</p>
+                        <button className="button">DEVSTUDIO</button>
+                    </div>
+                </ul>
+            </nav>
+         );
+    }
+}
+ 
+export default Navbar;
